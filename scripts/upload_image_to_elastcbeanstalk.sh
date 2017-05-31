626430d9bfdf22a1da82d41c14312ee6d1783aa8
e-novinfo <webmaster@e-novinfo.ch>
2017-05-31 14:58:52 +0200

    Change sh

--- scripts/upload_image_to_elastcbeanstalk.sh
+++ scripts/upload_image_to_elastcbeanstalk.sh
@@ -103,5 +103,5 @@ aws elasticbeanstalk describe-events
 
 echo "::::: Try EB :::::"
 
-eb init docker-travis-php-test --region 13 --profile default
+eb init docker-travis-php-test --region us-east-2 --profile default
 eb local run

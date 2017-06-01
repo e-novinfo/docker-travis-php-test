4b5f6ef2f13fd8f2b7418c20220435a219a20462
e-novinfo <webmaster@e-novinfo.ch>
2017-06-01 14:00:27 +0200

    Change sh

--- scripts/upload_image_to_elastcbeanstalk.sh
+++ scripts/upload_image_to_elastcbeanstalk.sh
@@ -28,11 +28,13 @@ echo "::::: Creating .dockercfg file :::::"
 
 DOCKER_AUTH=($(sudo jq -r '.auths["https://index.docker.io/v1/"].auth' $DOCKER_CONFIG))
 
-echo "//Actual .docker/config.json"
+echo "//Auth value"
 echo $DOCKER_AUTH
-echo " "
+printf "\n"
 
+echo "//Actual .docker/config.json"
 sudo cat $DOCKER_CONFIG
+printf "\n"
 
 cat "$DOCKERCFG" \
   | sed 's|<DOCKER_AUTH>|'$DOCKER_AUTH'|g' \
@@ -41,14 +43,14 @@ cat "$DOCKERCFG" \
   | sed 's|<DOCKER_EMAIL>|'$DOCKER_EMAIL'|g' \
   > $DOCKERCFG
 
-sleep 10
+sleep 30
 
 echo "//Generated .dockerckfg"
 cat $DOCKERCFG
-echo " "
+printf "\n"
 
 aws s3 cp $DOCKERCFG s3://$EB_BUCKET/.dockercfg
-rm $DOCKERCFG
+sleep 30
 
 echo "::::: Creating Dockerrun.aws.json file :::::"
 
@@ -59,16 +61,14 @@ cat "$DOCKERRUN_FILE" \
   | sed 's|<TAG>|'$DOCKER_TAG'|g' \
   > $DOCKERRUN_FILE
 
-sleep 10
+sleep 30
 
 echo "//Generated Dockerrun.aws.json"
 cat $DOCKERRUN_FILE
-echo " "
+printf "\n"
 
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$PREFIX/$DOCKERRUN_FILE
-rm $DOCKERRUN_FILE
-
-sleep 10
+sleep 30
 
 echo "::::: Creating new Elastic Beanstalk version :::::"
 
@@ -79,7 +79,7 @@ aws elasticbeanstalk create-application-version \
   --version-label $DOCKER_TAG \
 	--source-bundle S3Bucket=$EB_BUCKET,S3Key=$PREFIX/$DOCKERRUN_FILE
   
-sleep 10  
+sleep 30  
 
 echo "::::: Updating Elastic Beanstalk environment :::::"
 
@@ -88,6 +88,12 @@ aws elasticbeanstalk update-environment \
   --environment-name $DEPLOYMENT_ENV_NAME \
   --application-name $APP_NAME \
   --version-label $DOCKER_TAG
+
+echo "::::: Removing file :::::"
+
+sleep 30  
+rm $DOCKERCFG
+rm $DOCKERRUN_FILE
 
 echo "::::: Describing application :::::"
 
